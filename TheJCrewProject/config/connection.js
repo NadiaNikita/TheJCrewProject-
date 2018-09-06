ba726cb0f9c74c35f42b8154d81ba61f36277e84
Jenn <jswan02@gmail.com>
2018-09-06 18:24:45 -0500

    project

--- TheJCrewProject/config/connection.js
+++ TheJCrewProject/config/connection.js
@@ -8,6 +8,21 @@ var connection = mysql.createConnection({
   database: "bottlesowine"
 });
 
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "bottlesowine",
+    });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
